3dc271b02bf33d3eeb20e09bdeeee48b3bcdc278
TadeuMartins <156713091+TadeuMartins@users.noreply.github.com>
2024-01-29 14:46:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ if DEBUG_LOGGING:
 
 # On Your Data Settings
 DATASOURCE_TYPE = os.environ.get("DATASOURCE_TYPE", "AzureCognitiveSearch")
-SEARCH_TOP_K = os.environ.get("SEARCH_TOP_K", 20)
+SEARCH_TOP_K = os.environ.get("SEARCH_TOP_K", 5)
 SEARCH_STRICTNESS = os.environ.get("SEARCH_STRICTNESS", 5)
 SEARCH_ENABLE_IN_DOMAIN = os.environ.get("SEARCH_ENABLE_IN_DOMAIN", "true")
 
