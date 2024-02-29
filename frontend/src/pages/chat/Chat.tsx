88b6b96830c59f844a77f8cc67fdb858470855d0
Tobias Doerper <tobias.doerper@web.de>
2024-02-29 10:03:36 +0100

    bug fix test try 6

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -713,7 +713,7 @@ const Chat = () => {
                             </Stack>
                             <QuestionInput
                                 clearOnSend
-                                placeholder="Type a new question...."
+                                placeholder="Type a new question..."
                                 disabled={isLoading}
                                 onSend={(question, id) => {
                                     appStateContext?.state.isCosmosDBAvailable?.cosmosDB ? makeApiRequestWithCosmosDB(question, id) : makeApiRequestWithoutCosmosDB(question, id)
